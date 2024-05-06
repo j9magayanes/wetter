4d252b07a8bab1b290e7366091e6b40d9ad8e5c1
janine <janine@upday.com>
2024-05-06 19:49:34 +0200

    feat:adjusted format

--- chart.js
+++ chart.js
@@ -1,163 +1,104 @@
- // set the dimensions and margins of the graph
- var margin = {
-  top: 20,
-  right: 20,
-  bottom: 30,
-  left: 50
-},
-width = 960 - margin.left - margin.right,
-height = 500 - margin.top - margin.bottom;
+// Set the dimensions and margins of the graph
+var margin = { top: 20, right: 20, bottom: 30, left: 50 };
+var width = 960 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
 
-// set the ranges
+// Set the ranges
 var x = d3.scaleLinear().range([0, width]);
 var y = d3.scaleLinear().range([height, 0]);
 
-// define the line  
-function basicy() {
-var ret = d3.line()
-  .x(function (d) {
-      return x(d.year);
-  })
-return ret;
-}
-var valueline = basicy()
-.y(function (d) {
-  return y(d.GNI);
-});
-var valueline2 = basicy()
-.y(function (d) {
-  return y(d.PGDI);
-});
-var valueline3 = basicy()
-.y(function (d) {
-  return y(d.PPP);
-});
-var valueline4 = basicy()
-.y(function (d) {
-  return y(d.xyz);
-});
-var div = d3.select("#chart").append("div")
-.attr("class", "tooltip")
-.style("opacity", 0);
-
-// append the svg obgect to the body of the page
-// appends a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
-var svg = d3.select("#chart").append("svg")
-.attr("width", width + margin.left + margin.right)
-.attr("height", height + margin.top + margin.bottom)
-.append("g")
-.attr("transform",
-  "translate(" + margin.left + "," + margin.top + ")");
-
-// Get the data
-var datalist = [];
-d3.csv("data.csv", function (error, data) {
-if (error) throw error;
-// scale the range of the data
-x.domain(d3.extent(data, function (d) {
-  return d.year;
-}));
-y.domain([0, d3.max(data, function (d) {
-  return Math.max(d.GNI, d.PGDI, d.PPP,d.xyz);
-})]);
-
-// add the valueline path.
-//그냥 data를 하게 되면 array[16]이 되지만, [data]를 하게 되면 array[1]안에 있는 array[16]이 된다. 
-var dataArray = [{
-  "name": "GNI",
-  "x": 100,
-  "y": 50,
-  "class": "line-text",
-  "class2": "line",
-  "dataline": valueline
-}, {
-  "name": "PGDI",
-  "x": 100,
-  "y": 70,
-  "class": "line2-text",
-  "class2": "line2",
-  "dataline": valueline2
-}]
-for (var i = 0; i < dataArray.length; i++) {
-  svg.append("text").text(dataArray[i].name)
-      .attr("x", dataArray[i].x)
-      .attr("y", dataArray[i].y);
-  svg.append("rect")
-      .attr("x", dataArray[i].x - 70)
-      .attr("y", dataArray[i].y - 11)
-      .attr("width", 50)
-      .attr('height', 10)
-      .attr('class', dataArray[i].class)
-  svg.append("path")
-      .data([data])
-      .attr("class", dataArray[i].class2)
-      .attr("d", dataArray[i].dataline) 
+// Define the line generator function
+function lineGenerator() {
+    return d3.line().x(function (d) { return x(d.year); });
 }
 
-// add the dots with tooltips
-var fixeddot = svg.selectAll("dot")
-  .data(data)
-  .enter().append("circle")
-  .attr("r", 5)
-var fixeddot2 = svg.selectAll("dot")
-  .data(data)
-  .enter().append("circle")
-  .attr("r", 5)
-var fixeddot3 = svg.selectAll("dot")
-  .data(data)
-  .enter().append("circle")
-  .attr("r", 5)
-  var fixeddot4 = svg.selectAll("dot")
-  .data(data)
-  .enter().append("circle")
-  .attr("r", 4)
+// Define line generators for different datasets
+var valueline = lineGenerator().y(function (d) { return y(d.GNI); });
+var valueline2 = lineGenerator().y(function (d) { return y(d.PGDI); });
 
-fixeddot.attr("cx", function (d) {
-      return x(d.year);
-  })
-  .attr("cy", function (d) {
-      return y(d.GNI);
-  })
-  .on("mouseover", function (d) {
-      div.transition()
-          .duration(200)
-          .style("opacity", .9);
-      div.html("<p>년도:" + d.year + "</p> <p>GNI:" + d.GNI + "</p>")
-          .style("left", (d3.event.pageX) + "px")
-          .style("top", (d3.event.pageY - 28) + "px");
-  });
+// Create a tooltip div
+var div = d3.select('#chart').append('div')
+    .attr('class', 'tooltip')
+    .style('opacity', 0);
 
-fixeddot2.attr("cx", function (d) {
-      return x(d.year);
-  })
-  .attr("cy", function (d) {
-      return y(d.PGDI);
-  })
-  .on("mouseover", function (d) {
-      div.transition()
-          .duration(200)
-          .style("opacity", .9);
-      div.html("<p>년도:" + d.year + "</p> <p>PGDI:" + d.PGDI + "</p>")
-          .style("left", (d3.event.pageX) + "px")
-          .style("top", (d3.event.pageY - 28) + "px");
-  });
+// Append the SVG object to the chart div
+var svg = d3.select('#chart').append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-// add the X Axis
-svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x));
-  svg.append("g")
-  .attr("class", "grid")
-  .call(d3.axisLeft(y)
-          .tickSize(-width)
-          .tickFormat("")
-  );
+// Load data from CSV file
+d3.csv('data.csv', function (error, data) {
+    if (error) throw error;
+    
+    // Scale the range of the data
+    x.domain(d3.extent(data, function (d) { return d.year; }));
+    y.domain([0, d3.max(data, function (d) { return Math.max(d.GNI, d.PGDI, d.PPP, d.xyz); })]);
 
-// add the Y Axis
-svg.append("g")
-  .call(d3.axisLeft(y))
-  .select('.domain')
-  .attr('stroke-width', 0);
+    // Add the valueline paths
+    var dataArray = [
+        { name: '30 TAGE 2023/2024', x: 70, y: 550, class: 'line-text', class2: 'line', dataline: valueline },
+        { name: '30 TAGE 1973/2022', x: 500, y: 550, class: 'line2-text', class2: 'line2', dataline: valueline2 }
+    ];
+    dataArray.forEach(function (dataItem) {
+        svg.append('text').text(dataItem.name)
+            .attr('x', dataItem.x).attr('y', dataItem.y);
 
+        svg.append('rect').attr('x', dataItem.x - 70).attr('y', dataItem.y - 11)
+            .attr('width', 50).attr('height', 10).attr('class', dataItem.class);
+
+        svg.append('path').data([data]).attr('class', dataItem.class2).attr('d', dataItem.dataline);
+    });
+
+    // Add the dots with tooltips
+    var dots = svg.selectAll('.dot')
+        .data(data)
+        .enter().append('circle')
+        .attr('class', 'dot')
+        .attr('r', 5)
+        .on('mouseover', function (d) {
+            div.transition().duration(200).style('opacity', 0.9);
+            div.html('<p>년도:' + d.year + '</p> <p>GNI:' + d.GNI + '</p>')
+                .style('left', d3.event.pageX + 'px').style('top', (d3.event.pageY - 28) + 'px');
+        });
+
+    // Position dots based on data
+    dots.attr('cx', function (d) { return x(d.year); })
+        .attr('cy', function (d) { return y(d.GNI); });
+
+    // Add the X Axis
+    svg.append('g').attr('transform', 'translate(0,' + height + ')')
+        .style('stroke-dasharray', '1 10').call(d3.axisBottom(x));
+
+    // Add the Y Axis and grid lines
+    svg.append('g').attr('class', 'grid')
+        .call(d3.axisLeft(y).tickSize(-width).tickFormat(''));
+    
+    svg.append('g').call(d3.axisLeft(y))
+        .select('.domain').attr('stroke-width', 0);
+           // Add the area
+    svg.append("path")
+    .datum(data)
+    .attr("fill", "red")
+    .attr("fill-opacity", .3)
+    .attr("stroke", "none")
+    .attr("d", d3.area()
+      .x(function(d) { return x(d.year) })
+      .y0( height )
+      .y1(function(d) { return y(d.GNI) })
+      )
+      svg.append("path")
+      .datum(data)
+      .attr("fill", "SKYBLUE")
+      .attr("fill-opacity", 0.7)
+      .attr("stroke", "none")
+      .attr("d", d3.area()
+        .x(function(d) { return x(d.year) })
+        .y0( height )
+        .y1(function(d) { return y(d.PGDI) })
+        )
 });
+
+ 
+ 
